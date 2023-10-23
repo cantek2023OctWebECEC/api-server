aa575d5e8eff67e91798a7be18dfa603fdba1ee8
DrEdwardPCB <eternal.edward1997@gmail.com>
2023-10-23 12:07:31 -0400

    added entity

--- src/database/postgres/entities/Comment.ts
+++ src/database/postgres/entities/Comment.ts
@@ -1,14 +1,27 @@
-import { CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import { Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, OneToMany, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import User from "./User";
+import Trip from "./Trip";
 
 @Entity()
 export class Comment {
 	@PrimaryGeneratedColumn("uuid")
 	id: string;
 
+	@Column({ type: "text", nullable: false })
+	content: string;
+
 	@CreateDateColumn()
 	createdAt: Date;
 
 	@UpdateDateColumn()
 	updatedAt: Date | null;
+
+	@ManyToOne((type)=>User, (user)=>user.comments)
+	@JoinColumn()
+	user:User
+	
+	@ManyToOne((type)=>Trip,(trip)=>trip.comments)
+	@JoinColumn()
+	trip:Trip
 }
 export default Comment;
--- src/database/postgres/entities/Trip.ts
+++ src/database/postgres/entities/Trip.ts
@@ -1,5 +1,8 @@
-import { CreateDateColumn, Entity, ManyToMany, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import { CreateDateColumn, Entity, ManyToMany, OneToMany, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
 import Attraction from "./Attraction";
+import Todo from "./Todo";
+import User from "./User";
+import Comment from "./Comment";
 
 @Entity()
 export class Trip {
@@ -14,5 +17,14 @@ export class Trip {
 
 	@ManyToMany((type) => Attraction, (attraction) => attraction.trips)
 	attractions: Attraction[];
+
+	@OneToMany((type) => Todo, (todo) => todo.trip)
+	todos: Todo[];
+
+	@OneToMany((type) => Comment, (comment) => comment.user)
+	comments: Comment[];
+
+	@ManyToMany((type) => User, (user) => user.jointTrip)
+	participants: User[];
 }
 export default Trip;
